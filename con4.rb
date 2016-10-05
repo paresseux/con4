1df3076f55595a663db6e0f05efc2ded3d29845f
ne <apopov98@gmail.com>
2016-10-05 08:15:15 +0300

    Add files via upload

--- con4.rb
+++ con4.rb
@@ -1,16 +1,17 @@
-Shoes.app :width => 900, :height => 625 do
+Shoes.app :width => 900, :height => 725 do
   blue = "#0000FF"
   red = rgb(255,0,0)
   black = rgb(0,0,0) 
+  yellow = rgb(255,255,0)
  
   
   @board = []
-  @columns = [[],[],[],[],[],[],[]]
+  @columns = [[],[],[],[],[],[],[],[]]
   @picked = []
   @turn = 0
 
-  6.times do |row|
-    7.times do |column|
+  7.times do |row|
+    8.times do |column|
       r = rect 100 * column+5,
                100 * row+5, 100, 100, :fill => blue
       @board << r
@@ -25,20 +26,24 @@ Shoes.app :width => 900, :height => 625 do
     row = y / 100
 
     if button == 1
-      space = (@columns[column].last*7)+column
+      space = (@columns[column].first*8)+column
       if !@picked.include?(space)
+	    
         @turn+=1
 		
-		if (@turn % 2 == 1)
-		  color = red
-		else
+		if (@turn % 3 == 2)
 		  color = black
+		else
+		  color = red
+		end
+		if (@turn % 3 == 0)
+		  color = yellow
 		end
 		
 		
         @board[space].style(:fill => color)
         @picked << space
-        @columns[column].pop
+        @columns[column].shift
         if solved?(color)
           alert("#{color} wins!")
         end
@@ -47,7 +52,7 @@ Shoes.app :width => 900, :height => 625 do
   end
 
   def solved?(color)
-    if vertical_win(color) || horizontal_win(color) || diagonal_win(color)
+    if vertical_win(color) || horizontal_win(color)
       return true
     else
       return false
@@ -55,11 +60,11 @@ Shoes.app :width => 900, :height => 625 do
   end
 
   def horizontal_win(color)
-    6.times do |row|
-      current_row = @board[(row*7)..(row*7+6)]
+    7.times do |row|
+      current_row = @board[(row*8)..(row*8+7)]
       continuous_row_count = 0
       position = 0
-      while(position < current_row.length && continuous_row_count != 4) do
+      while(position < current_row.length && continuous_row_count != 5) do
         if(current_row[position].style[:fill] == color)
           continuous_row_count += 1
         else
@@ -68,19 +73,7 @@ Shoes.app :width => 900, :height => 625 do
         position+= 1
       end
 
-      if continuous_row_count >= 4
-        return true
-      end
-    end
-    false
-  end
-
-  def vertical_win(color)
-    7.times do |column|
-      current_column = [0,7,14,21,28,35].collect{|num| @board[column+num]}
-      current_column = current_column.drop_while{|rect| rect.style[:fill] != color}
-      current_column = current_column.take_while{|rect| rect.style[:fill] == color}
-      if current_column.count >= 4
+      if continuous_row_count >= 5
         return true
       end
     end
@@ -88,4 +81,22 @@ Shoes.app :width => 900, :height => 625 do
   end
 
   
+  
+  def vertical_win(color)
+    8.times do |column|
+	  current_column = [0,8,16,24,32,40,48].collect{|num| @board[column+num]}
+      current_column = current_column.drop_while{|rect| rect.style[:fill] != color}
+      current_column = current_column.take_while{|rect| rect.style[:fill] == color}
+	  
+	  if current_column.count >= 5
+        return true
+      end
+	  
+	end
+		
+	false
+  end
+ 
+
+ 
 end
